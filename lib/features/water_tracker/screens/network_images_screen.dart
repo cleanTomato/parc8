ab512c278e89557e61ea13e12fd33264025ee69c
Anzdock <anzdock-azndock200@yandex.ru>
2025-11-07 16:01:17 +0300

    изучение проблемы зависимостей в проектах

--- lib/features/water_tracker/screens/network_images_screen.dart
+++ lib/features/water_tracker/screens/network_images_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 
+
 class NetworkImagesScreen extends StatelessWidget {
   const NetworkImagesScreen({super.key});
 
